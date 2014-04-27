e57682537147cfd1e602f79c7acfb600bec65e27
Vijay Patel <vijay@spiritedtechie.com>
2014-04-27 11:22:49 +0100

    Removed HTTP logging - not really needed as was only used for debugging and does not belong to this classes responsibility.

--- swagger-customer-api/src/main/java/lab/api/ApiOriginFilter.java
+++ swagger-customer-api/src/main/java/lab/api/ApiOriginFilter.java
@@ -1,14 +1,12 @@
 package lab.api;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
@@ -21,22 +19,6 @@ public class ApiOriginFilter implements javax.servlet.Filter {
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
-
-        final HttpServletRequest req = (HttpServletRequest) request;
-
-        LOG.info("**** HTTP Request Logging");
-        LOG.info("Context Path " + req.getServletContext().getContextPath());
-        LOG.info("Servlet Context Name: " + req.getServletContext().getServletContextName());
-        LOG.info("Request URI: " + req.getRequestURI());
-        LOG.info("Method: " + req.getMethod());
-
-        final Enumeration<String> headers = req.getHeaderNames();
-        while (headers.hasMoreElements()) {
-            final String headerName = headers.nextElement();
-            LOG.info("Header name: " + headerName + " , header value: " + req.getHeader(headerName));
-        }
-
-        LOG.info("**** HTTP Request Logging Complete");
 
         LOG.info("**** Setting access controls: ");
 
